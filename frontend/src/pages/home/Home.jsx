19b72665e720af6a54d0f47869a394a9338cf778
biswa-ganguly <biswaganguly16@gmail.com>
2024-06-11 11:45:52 +0530

    making it responsive

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,9 +3,9 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
 	return (
-		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			<Sidebar />
-			<MessageContainer />
+		<div className='flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] h-screen rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<Sidebar className='w-full sm:w-1/4 md:w-1/3 lg:w-1/4 xl:w-1/5'/>
+			<MessageContainer className='flex-grow'/>
 		</div>
 	);
 };
